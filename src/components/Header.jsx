245f3995cc825e0c6df0dd3977be76772fea865f
the-red-dot <theredot2023@gmail.com>
2025-03-18 06:07:12 +0200

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
+// src/components/Header.jsx
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 function Header({ currentLibrary, onBurgerClick, onLoginClick, user, onLogout }) {
+  const location = useLocation(); // Get the current route
+
+  // Determine the title based on the current route
+  const title = location.pathname.startsWith('/media') ? 'פרטי מדיה' : currentLibrary;
+
   return (
     <header>
       <div className="header-left">
@@ -31,7 +38,7 @@ function Header({ currentLibrary, onBurgerClick, onLoginClick, user, onLogout })
           )}
         </div>
       </div>
-      <h1 id="library-title">{currentLibrary}</h1>
+      <h1 id="library-title">{title}</h1>
       <button id="view-toggle-button" aria-label="Change View">
         <svg viewBox="0 0 24 24">
           <path d="M3 3h7v7H3V3zm0 11h7v7H3v-7zm11-11h7v7h-7V3zm0 11h7v7h-7v-7z" />
